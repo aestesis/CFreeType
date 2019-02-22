6f19849b1dee2fa72f153c99ddf5971c8855a71a
renan <renan@aestesis.org>
2019-02-22 05:47:12 +0100

    update swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,18 +1,14 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "Cfreetype",
-    pkgConfig: "freetype2",
-    providers: [ 
-        .apt(["freetype2"]) 
-    ],
     products: [
         .library(name: "Cfreetype",targets: ["Cfreetype"])
     ],
     dependencies: [
     ],
     targets: [
-        .target(name: "Cfreetype",dependencies: [])
+        .systemLibrary(name: "Cfreetype")
     ]
 )
